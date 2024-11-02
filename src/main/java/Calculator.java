1fa30be05fb7252633666e113735236932f264c2
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-11-02 09:54:40 -0300

    calculator tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,5 +8,48 @@ public class Calculator {
         return x - y;
     }
 
+    public int divide(int x, int y) {
+        if (y == 0) {
+            return 0;
+        } else {
+            return x / y;
+        }
+    }
 
+    public int multiply(int x, int y){
+        return x * y;
+    }
+
+    public boolean esPar(int x){
+        return x % 2 == 0;
+    }
+
+    // Conversión de Celsius a Fahrenheit
+    public static double convertirCelsiusAFahrenheit(double celsius) {
+        return (celsius * 1.8) + 32;
+    }
+
+    // Retorna verdadero si un numero es primo, falso en caso contrario
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1)
+            return false;
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Retorna verdadero si un numero es negativo
+    public boolean esNegativo(int x){
+        return x < 0;
+    }
+
+    // Retorna el valor absoluto de un numero entero
+    public int valorAbsoluto(int x){
+        if (x >= 0)
+            return x;
+        else
+            return -x;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -10,4 +10,157 @@ public class CalculatorTest {
         int result = calculator.addition(3, 2);
         assertEquals(5, result);
     }
+
+    @Test
+    public void additionWithNegativeNumbersTest() {
+        int result = calculator.addition(-3, 2);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void subtractionTest() {
+        int result = calculator.subtraction(3, 2);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void divideWithYDifferentThan0Test(){
+        int result = calculator.divide(6, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void divideWithYEquals0Test(){
+        int result = calculator.divide(6, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void multiplyTest(){
+        int result = calculator.multiply(2,3);
+        assertEquals(6,result);
+    }
+
+    @Test
+    public void esParConNumeroImparTest(){
+        boolean result = calculator.esPar(3);
+        assertFalse(result);
+    }
+
+    // Tests para la conversión de Celsius a Fahrenheit
+    @Test
+    public void convertirCelsiusAFahrenheitTest_0C() {
+        double result = calculator.convertirCelsiusAFahrenheit(0);
+        assertEquals(32.0, result);
+    }
+
+    @Test
+    public void convertirCelsiusAFahrenheitTest_40Cnegativo() {
+        double result = calculator.convertirCelsiusAFahrenheit(-40);
+        assertEquals(-40, result);
+    }
+    @Test
+    public void convertirCelsiusAFahrenheitTest_60Cnegativo() {
+        double result = calculator.convertirCelsiusAFahrenheit(-60);
+        assertEquals(-76, result);
+    }
+
+    // Tests para verificar si un número es primo
+    @Test
+    public void esPrimoTest_negativo1() {
+        boolean result = calculator.esPrimo(-1);
+        assertFalse(result);
+    }
+
+   @Test
+    public void esPrimoTest_0() {
+        boolean result = calculator.esPrimo(0);
+        assertFalse(result);
+    }
+
+   @Test
+    public void esPrimoTest_1() {
+        boolean result = calculator.esPrimo(1);
+        assertFalse(result);
+    }
+
+   @Test
+    public void esPrimoTest_2() {
+        boolean result = calculator.esPrimo(2);
+        assertTrue(result);
+    }
+
+    @Test
+    public void esPrimoTest_3() {
+        boolean result = calculator.esPrimo(3);
+        assertTrue(result);
+    }
+
+   @Test
+    public void esPrimoTest_4() {
+        boolean result = calculator.esPrimo(4);
+        assertFalse(result);
+    }
+
+    @Test
+    public void esPrimoTest_5() {
+        boolean result = calculator.esPrimo(5);
+        assertTrue(result);
+    }
+
+    @Test
+    public void esPrimoTest_13() {
+        boolean result = calculator.esPrimo(13);
+        assertTrue(result);
+    }
+
+  @Test
+    public void esPrimoTest_97() {
+        boolean result = calculator.esPrimo(97);
+        assertTrue(result);
+    }
+
+    @Test
+    public void esPrimoTest_1000() {
+        boolean result = calculator.esPrimo(1000);
+        assertFalse(result);
+    }
+
+    // Tests para esNegativo(x)
+    @Test
+    public void esNegativoConNumNegTest(){
+        boolean result = calculator.esNegativo(-1);
+        assertTrue(result);
+    }
+    @Test
+    public void esNegativoConNumPosTest(){
+        boolean result = calculator.esNegativo(1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void esNegativoConCeroTest(){
+        boolean result = calculator.esNegativo(0);
+        assertFalse(result);
+    }
+
+    // TESTS DE VALOR ABSOLUTO
+    @Test
+    public void valorAbsolutoConNumPosTest(){
+        int result = calculator.valorAbsoluto(1);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void valorAbsolutoConNumNegTest(){
+        int result = calculator.valorAbsoluto(-1);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void valorAbsolutoConCeroTest(){
+        int result = calculator.valorAbsoluto(0);
+        assertEquals(0,result);
+    }
+
 }
